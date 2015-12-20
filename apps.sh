d41c24b4feb29dcd20a5367ed39d888c525af625
Constantine Kiriaze <ckiriaze@gmail.com>
2015-12-20 15:57:23 -0800

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -2,61 +2,62 @@
 # Apps
 apps=(
   adobe-creative-cloud
-  dropbox
-  google-chrome
-  firefox
-  screenflick
-  slack
-  transmit
-  appcleaner
-  spotify
-  vagrant
-  flash
-  iterm2
-  qlcolorcode
-  qlstephen
-  qlmarkdown
-  quicklook-json
-  qlprettypatch
-  quicklook-csv
-  betterzipql
-  webpquicklook
-  qlimagesize
-  suspicious-package
-  asepsis
-  sublime-text3
-  virtualbox
-  atom
-  flux
-  mailbox
-  sketch
-  tower
-  vlc
-  cloudup
-  nvalt
-  skype
-  transmission
   anvil
+  appcleaner
+  asepsis
+  atom
+  betterzipql
   brackets
   caffeine
   carbon-copy-cloner
+  cloudup
+  codekit
   cyberduck
+  dropbox
   droplr
+  firefox
+  flash
   fluid
+  flux
+  github-desktop
+  githubpulse
+  google-chrome
   harvest
+  iterm2
   keka
+  mailbox
   miro
-  mou
   moom
+  mou
   namechanger
+  nvalt
   openoffice
+  qlcolorcode
+  qlimagesize
+  qlmarkdown
+  qlprettypatch
+  qlstephen
+  quicklook-csv
+  quicklook-json
+  screenflick
+  sequel-pro
+  sketch
+  skype
+  slack
   smcfancontrol
   sourcetree
-  unrarx
-  sequel-pro
-  github
-  codekit
+  spotify
+  sublime-text3
+  suspicious-package
   tomighty
+  tower
+  transmission
+  transmit
+  unrarx
+  vagrant
+  virtualbox
+  vlc
+  webpquicklook
 )
 
 # Install apps to /Applications
